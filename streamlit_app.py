683c7a6aa859ad6fbf5a6a6156fabede3b0a6a1f
xu yirong <120074381+handsomexyr@users.noreply.github.com>
2024-07-15 16:38:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,70 @@
+from transformers import AutoModel, AutoTokenizer
 import streamlit as st
+from peft import PeftModel
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+st.set_page_config(
+    page_title="ChatGLM2-6b 演示",
+    page_icon=":robot:",
+    layout='wide'
 )
+
+
+@st.cache_resource
+def get_model():
+    model_path = "../chatglm2-6b"
+    model = AutoModel.from_pretrained(model_path, trust_remote_code=True).half().cuda()
+    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+# 加载 label lora权重
+    model = PeftModel.from_pretrained(model, './output/label_xfg').half()
+    return tokenizer, model
+
+
+tokenizer, model = get_model()
+
+st.title("ChatGLM2-6B")
+
+max_length = st.sidebar.slider(
+    'max_length', 0, 32768, 8192, step=1
+)
+top_p = st.sidebar.slider(
+    'top_p', 0.0, 1.0, 0.8, step=0.01
+)
+temperature = st.sidebar.slider(
+    'temperature', 0.0, 1.0, 0.8, step=0.01
+)
+
+if 'history' not in st.session_state:
+    st.session_state.history = []
+
+if 'past_key_values' not in st.session_state:
+    st.session_state.past_key_values = None
+
+for i, (query, response) in enumerate(st.session_state.history):
+    with st.chat_message(name="user", avatar="user"):
+        st.markdown(query)
+    with st.chat_message(name="assistant", avatar="assistant"):
+        st.markdown(response)
+with st.chat_message(name="user", avatar="user"):
+    input_placeholder = st.empty()
+with st.chat_message(name="assistant", avatar="assistant"):
+    message_placeholder = st.empty()
+
+prompt_text = st.text_area(label="用户命令输入",
+                           height=100,
+                           placeholder="请在这儿输入您的命令")
+
+button = st.button("发送", key="predict")
+
+if button:
+    input_placeholder.markdown(prompt_text)
+    history, past_key_values = st.session_state.history, st.session_state.past_key_values
+    for response, history, past_key_values in model.stream_chat(tokenizer, prompt_text, history,
+                                                                past_key_values=past_key_values,
+                                                                max_length=max_length, top_p=top_p,
+                                                                temperature=temperature,
+                                                                return_past_key_values=True):
+        message_placeholder.markdown(response)
+
+    st.session_state.history = history
+    st.session_state.past_key_values = past_key_values
